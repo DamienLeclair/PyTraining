0137860b2d55da5835ddecb7c67370856f8d49b1
Damien Leclair <damien.leclair@satimo.fr>
2015-05-07 13:08:56 +0200

    Finished exceptions tuto

--- PyTraining/exceptions.py
+++ PyTraining/exceptions.py
@@ -1,15 +1,62 @@
 __author__ = 'dleclair'
-try:
-    value = input("Enter an interger :");
-    integer = int(value)
-    # print(value, "is an integer")
-# except:
-#     print(value, "is not an integer")
+
+# #
+# # Catching exceptions sample
+# #
+# try:
+#     value = input("Enter an integer : ");
+#     integer = int(value)
+#     print(value, "is an integer")
+#
+# # Catch only ValueError
 # except ValueError:
 #     print(value, "is not an integer")
+#
+# # Add a variable on ValueError exception
+# except ValueError as ex:
+#     print(ex)
+#
+# # pass is used to keep empty an exception or function
+# # it's not recommended to use it
+# except ValueError as ex:
+#     pass
+#
+# # Catch all exceptions
+# except:-
+#     print(value, "is not an integer")
+#
+# # Do the job if no error occurred
+# # it's not recommended to use it
+# else:
+#     print(value, "is an integer")
+#
+# # classical finally block
+# finally:
+#     print("End of try block")
+
+# #
+# # Assertion sample
+# #
+# try:
+#     value = input("Enter a positive integer : ")
+#     integer = int(value)
+#     assert integer >= 0
+#     print(value, " is a positive integer")
+# except ValueError:
+#     print(value, "is not an integer")
+# except AssertionError:
+#     print(value, "is not positive")
+
+#
+# Raise an exception
+#
+try:
+    value = input("Enter a positive integer : ")
+    integer = int(value)
+    if integer < 0:
+        raise ValueError("{0} is not a positive integer".format(integer))
+    print(value, " is a positive integer")
 except ValueError as ex:
     print(ex)
-else:
-    print(value, "is an integer")
-finally:
-    print("End of try block")
+except AssertionError:
+    print(value, "is not positive")
